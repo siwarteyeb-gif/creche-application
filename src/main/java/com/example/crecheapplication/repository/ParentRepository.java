61f3ae802f6e130712ea954b52b948b25b627cff
TEYEB Wissem <wissem.teyeb@supcom.tn>
2026-01-14 18:49:07 +0100

    Ajout des 3 tables

--- src/main/java/com/example/crecheapplication/repository/ParentRepository.java
+++ src/main/java/com/example/crecheapplication/repository/ParentRepository.java
@@ -3,5 +3,8 @@ package com.example.crecheapplication.repository;
 import com.example.crecheapplication.model.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ParentRepository extends JpaRepository<Parent,Long> {
+    Optional<Parent> findByNomAndPrenom(String nom, String prenom);
 }
--- src/main/java/com/example/crecheapplication/service/ParentService.java
+++ src/main/java/com/example/crecheapplication/service/ParentService.java
@@ -4,8 +4,24 @@ import com.example.crecheapplication.model.Parent;
 import com.example.crecheapplication.repository.ParentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class ParentService {
 
+
+    @Autowired private ParentRepository parentRepository;
+    public Parent inscrire(String nom, String prenom, String email, String telephone, String password)
+    {
+        if (parentRepository.findByNomAndPrenom(nom, prenom).isPresent())
+    { throw new RuntimeException("Un compte avec ce nom, prénom existe déjà !");
+    } Parent parent = new Parent();
+        parent.setNom(nom);
+        parent.setPrenom(prenom);
+        parent.setEmail(email);
+        parent.setTelephone(telephone);
+        parent.setPassword(password);
+        parent.setModifiedAt(LocalDateTime.now());
+        parent.setCreatedAt(LocalDateTime.now());
+        return parentRepository.save(parent); }
 }
